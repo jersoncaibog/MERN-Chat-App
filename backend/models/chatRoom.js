0c7df2f0bf97c2b2529ca3d84d2cb5133935ac57
Jerson <jersoncaibog0423@gmail.com>
2025-01-04 21:46:09 +0800

    DELETE API endpoint

--- backend/models/chatRoom.js
+++ backend/models/chatRoom.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const chatRoomSchema = new mongoose.Schema({
-    // members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    members: [{ type: String, required: true }],
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 }, {
     timestamps: true,
 });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
 import { connectDB } from './config/database.js';
-import ChatRoom from './models/chatRoom.js'
+import ChatRoom from './models/chatRoom.js';
 
 dotenv.config()
 
@@ -19,17 +19,34 @@ app.post('/api/chatrooms', async (req, res) => {
     const { members } = req.body;
 
     if (!members) {
-        return res.status(400).json({ success: 'false', message: 'Chatroom members are required' });
+        return res.status(400).json({ success: false, message: 'Chatroom members are required' });
     }
 
     const newChatRoom = new ChatRoom({ members })
     
     try {
         await newChatRoom.save();
-        res.status(201).json({ success: 'true', data: newChatRoom });
+        res.status(201).json({ success: true, data: newChatRoom });
     } catch (err) {
         console.error('Error creating chatroom', err);
-        res.status(500).json({ success: 'false', message: 'Server error' });
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+})
+
+app.delete('/api/chatrooms/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    try {
+        const chatroom = await ChatRoom.findByIdAndDelete(id);
+        
+        if (!chatroom) {
+            return res.status(404).json({ success: false, message: 'Chatroom not found' });  // If chatroom not found
+        }
+        
+        res.status(200).json({ success: true, message: 'Chatroom deleted successfully' });  // Success response
+    } catch (err) {
+        console.error('Error deleting chatroom:', err);
+        res.status(500).json({ success: false, message: 'Server error' });  // Error response
     }
 })
 
