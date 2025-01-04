2da7b577e2a528885bd626d5fe9fa83fa400618e
Jerson <jersoncaibog0423@gmail.com>
2025-01-05 02:53:38 +0800

    all

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css'
+import { Box } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router'
+import Home from '@/components/pages/Home.tsx'
 
 function App() {
-
-  
-  
   return (
-    <>
- 
-    </>
+    <Box minH={"100vh"}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Box>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from '@/components/ui/provider.tsx'
+import { BrowserRouter } from "react-router";
+import { ColorModeProvider } from "@/components/ui/color-mode"
+
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider>
+        <ColorModeProvider>
+          <App />
+        </ColorModeProvider>
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
